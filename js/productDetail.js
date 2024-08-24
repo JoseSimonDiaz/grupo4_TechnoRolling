7f0b33eedfe24d93be33386cef2695eca1e91ab0
JoseSimonDiaz <josesimondiaz13@gmail.com>
2024-08-24 19:37:11 -0300

    modifique el boton del index.html

--- js/productDetail.js
+++ js/productDetail.js
@@ -7,34 +7,6 @@ const showProductId = () => {
   return producto
 };
 
-// showProductId();
-
-// /* renderizarProductos*/
-
-// const renderizarProducto = (producto) => {
-//   const { id, nombre, imagen, precio, stock,descripcion } = producto;
-
-//   return /* HTML*/` <section class="detalleProducto" >
-//                 <div class="conteiner_image">
-//                   <img src="${imagen}" alt="imagenProduct">                  
-//                 </div>
-//                     <div class="detalleDescripcion">
-//                       <h1>${nombre}</h1>
-//                         <h3>${descripcion}</h3>
-//                        <p>Cantidad en stock: <span id="span">${stock} </span></p>
-//                        <p>Categoria: <span id="span"></span></p>
-//                         <div class="precio">
-//                           <p>$<span>${precio}</span></p>
-//                           <button class="comprar">COMPRAR</button>
-//                         </div>
-//                         <div class="envios">
-//                           <i  class='bx bxs-truck bx-lg'></i>
-//                           <h3>Envios a todo el pais</h3>
-//                         </div>
-//                     </div>
-//            <div id="id"></div>
-//           </section>`
-
 
 // renderizarProducto()
 
@@ -65,6 +37,9 @@ productIdHTML.innerHTML += `
           </section>`
      
 };
+
+
+
 
 renderProduct();
 
